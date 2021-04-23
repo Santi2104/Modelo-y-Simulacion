ca77ea276cdeafad564a652759090db93afc058e
Santi2104 <santif458@gmail.com>
2021-04-22 23:01:49 -0300

    Codigo un poco mas refinado, aun con algunos problemas

--- TP 1/index.js
+++ TP 1/index.js
@@ -1,39 +1,44 @@
-//let ps = true;
-let q = 3;
+let cola = [1,2,3];
 let tiempo = 1;
-let actual = 0;
+let tiempo_servidor = 0;
 let atendidos = 0;
 
-
+//Problema 1: 
 
 while (tiempo <= 3600 ) {
     
     if(tiempo == 1)
     {
-        q--; //Quitamos un cliente de la cola
-        actual = 1; //El servidor comieza a atender
+
+        cola.shift();//Quitamos un cliente de la cola
+        tiempo_servidor = 1; //El servidor comieza a atender
     }
 
     if(tiempo % 45 == 0) //Si pasaron 45 seg, llega un cliente a la fila
     {
-        q++;
+        if(cola.sort == 0){ //si la cola esta vacia, hacemos como que agregamos y ahi no mas lo mandamos al servidor
+            tiempo_servidor = 1; //Servidor comienza a atender
+        }else{  //Si no esta vacia, agregamos un cliente normalmente
+            cola.push(tiempo);
+        }
+        
     }
     
-    if(actual % 40 == 0)//Si pasaron 40 seg
+    if(tiempo_servidor % 40 == 0 && cola.sort() != 0)//Si pasaron 40 seg y la cola no esta vacia
     {
         atendidos ++;//Se termino de atender a un cliente
-        q--;//Inmediatamente un cliente toma su lugar 
-        actual = 1;//El servidor cominza a antender
+        cola.shift();//Inmediatamente un cliente toma su lugar 
+        tiempo_servidor = 1;//El servidor cominza a antender
 
     }
-    //console.log(actual % 45);
-    actual++; //Aumentamos el tiempo del servidor
+
+    tiempo_servidor++; //Aumentamos el tiempo del servidor
     tiempo++;//Aumentamos el tiempo de la simulacion
 
 }
 
 console.log('personas atendidas', atendidos);
-console.log('Personas en la cola', q);
+console.table(cola);
 
     
 
